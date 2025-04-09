8e228a9afafd5e60c3be9c0ba28f44448acf4d7d
Aykun <115653364+akun-n@users.noreply.github.com>
2025-04-09 22:18:27 +0300

    Added project

---
+++ public/script.js
@@ -0,0 +1,37 @@
+const elt = document.getElementById('calculator');
+const calculator = Desmos.GraphingCalculator(elt, {showGrid: false, showXAxis: false, showYAxis: false});
+calculator.setMathBounds({
+    left: -5.954,
+    right: 6.36,
+    bottom: -4.847,
+    top: 4.938
+});
+fetch('watchModel.json')
+    .then(response => response.json())
+    .then(data => {
+        calculator.setState(data);
+
+        function calculateInitialN() {
+            const now = new Date();
+            const hour = now.getHours();
+            const minutes = now.getMinutes();
+            const seconds = now.getSeconds();
+            console.log('Hour:', hour);
+            console.log('Minutes:', minutes);
+            console.log('Seconds:', seconds);
+
+            return 1233.0752 - ((6.2832 * 60 * hour) + (6.2832 * minutes) + ((6.2832/60) * seconds));
+        }
+
+        let n = calculateInitialN();
+
+        calculator.setExpression({ id: 'n', latex: `n=${n}` });
+
+        setInterval(() => {
+            n -= 0.020944;
+            calculator.setExpression({ id: 'n', latex: `n=${n}` });
+        }, 200);
+    })
+    .catch(error => {
+        console.error('Error loading graph state:', error);
+    });
